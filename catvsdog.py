de36da154dbed26aecdca7b69679ec8504e78b99
David Weber <dwebe003@ucr.edu>
2017-11-05 09:34:30 -0800

    fixed graph

--- catvsdog.py
+++ catvsdog.py
@@ -29,14 +29,26 @@
 #				3) Do the above, conversely, if voting FOR dog and AGAINST cat.
 #
 #########################################################################################
+class BiGraph:
+	def __init__(self):
+		self.left = []
+		self.right = []
+		self.edges = []
+		
+	def addleft(self, vote):
+		self.left.append(vote)
+	
+	def addright(self, vote):
+		self.right.append(vote)
 #########################################################################################
 
 def voter(c, d, v):
 
 	# initialize cats and dogs arrays of size c and d, respectively, and set entries to 0.
 	# these will store the voting scores for each cat and dog.
-	cats = [0] * c
-	dogs = [0] * d
+	G = BiGraph()
+	catVotes = []
+	dogVotes = []
 	satisfied = 0
 	
 	# iterates below process for each voter
@@ -46,6 +58,8 @@ def voter(c, d, v):
 		arr = raw_input().split()
 		string1 = arr[0]
 		string2 = arr[1]
+
+		vote = (string1, string2)
 		
 		# checks same species has not been entered, e.g. C1 C2 or D1 D1
 		if(string1[0] == string2[0]):
@@ -55,35 +69,32 @@ def voter(c, d, v):
 		
 		# checks if voter is cat person
 		elif(string1[0] == 'C' or string1[0] == 'c'):
-			# determines which cat and dog we are going to work with
-			x = int(string1[1])
-			y = int(string2[1])
+			G.addleft(vote)
+			catVotes.append(vote)
 			
-			# increments score of specified cat
-			cats[x-1] += 1
-			# decrements score of specified dog
-			dogs[y-1] -= 1
-			
-			# if specified cat is scoring positively && specified dog is not positive,
-			# increment satisfied
-			if(cats[x-1] > 0 and dogs[y-1] <= 0):
+			if len(dogVotes) == 0:
 				satisfied += 1
+			else:
+				for x in range(len(dogVotes)):
+					if( (vote[0][1] == G.right[x][1][1]) or (vote[1][1] == G.right[x][0][1]) ):
+						continue
+					else:
+						satisfied += 1
+			
 				
 		# checks if voter is dog person	
 		elif(string1[0] == 'D' or string1[0] == 'd'):
-			# determines which cat and dog we are going to work with
-			x = int(string1[1])
-			y = int(string2[1])
+			G.addright(vote)
+			dogVotes.append(vote)
 			
-			# increments score of specified dog
-			dogs[x-1] += 1
-			# decrements score of specified cat
-			cats[y-1] -= 1
-			
-			# if specified dog is scoring positively && specified cat is not positive,
-			# increment satisfied
-			if(dogs[x-1] > 0 and cats[y-1] <= 0):
+			if len(catVotes) == 0:
 				satisfied += 1
+			else:
+				for x in range(len(catVotes)):
+					if( (vote[0][1] == G.left[x][1][1]) or (vote[1][1] == G.left[x][0][1]) ):
+						continue
+					else:
+						satisfied += 1
 			
 		else:
 			print("Error: recast please...\n")
